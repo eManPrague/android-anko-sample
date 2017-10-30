f3f2647b6c087c9231bd914722d6af51198566a5
vaclav.souhrada <vaclav.souhrada@eman.cz>
2017-10-30 12:53:10 +0100

    Added Async calls by using Anko Coroutines

--- app/src/main/kotlin/cz/eman/android/sample/anko/sign_in/SignInActivity.kt
+++ app/src/main/kotlin/cz/eman/android/sample/anko/sign_in/SignInActivity.kt
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import cz.eman.android.sample.anko.sign_in.bl.ISignInBL
 import cz.eman.android.sample.anko.sign_in.bl.SignInBL
 import cz.eman.android.sample.anko.sign_in.model.AuthCredentials
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
 import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.setContentView
 import org.jetbrains.anko.toast
@@ -16,23 +20,23 @@ import org.jetbrains.anko.toast
  */
 class SignInActivity : AppCompatActivity() {
 
-  private val signInBL: ISignInBL = SignInBL()
-  private lateinit var view: SingInView
+    private val signInBL: ISignInBL = SignInBL()
+    private lateinit var view: SingInView
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    view = SingInView()
-    view.setContentView(this)
-  }
-
-  fun authorizeUser(username: String, password: String) {
-
-    doAsync {
-      val authorized = signInBL.checkUserCredentials(AuthCredentials(username = username, password = password))
-
-      activityUiThread {
-        if (authorized) toast("Signed!!!") else view.showAccessDeniedAlertDialog()
-      }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        view = SingInView()
+        view.setContentView(this)
     }
-  }
+
+    fun authorizeUser(username: String, password: String) {
+        async(UI) {
+            val authorized: Deferred<Boolean> = bg {
+                signInBL.checkUserCredentials(AuthCredentials(username = username, password = password))
+            }
+
+            if (authorized.await()) toast("Signed!!!") else view.showAccessDeniedAlertDialog()
+        }
+    }
+
 }
